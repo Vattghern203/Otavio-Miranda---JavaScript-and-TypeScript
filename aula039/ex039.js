ee0b0c796b99ee8c5c7f09dc0899b89eb43ac52f
vattghern <otavioaugustobrb19@gmail.com>
2022-08-15 16:48:32 -0300

    While and Do While

---
+++ aula039/ex039.js
@@ -0,0 +1,13 @@
+function random(min, max) {
+    const randomNumber = Math.random() * (max - min) + min
+    return Math.floor(randomNumber)
+}
+
+const min = 1
+const max = 50
+let rand = random(min, max)
+
+while (rand !== 10) {
+    rand = random(min, max)
+    console.log(rand)
+}
