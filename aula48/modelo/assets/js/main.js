dd4b97ca862e7402c5eb4e3b4df0e3dbe7b65b6f
vattghern <otavioaugustobrb19@gmail.com>
2022-08-23 01:21:37 -0300

    Module 1, Last exercise

--- aula48/modelo/assets/js/main.js
+++ aula48/modelo/assets/js/main.js
@@ -1,19 +1,77 @@
-const taskBtn = document.querySelector('#insert-task')
-const taskList = document.querySelector('#list-task')
-const taskAdd = document.getElementById('add-task')
+const inputTask = document.querySelector('.input-task')
+const btnTask = document.querySelector('.btn-add-task')
+const taskList = document.querySelector('.list-task')
 
-const addTask = () => {
-    const newTask = document.createElement("li")
-    const newBtn = document.createElement("button")
+inputTask.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {{
+       if (!inputTask.value) return
 
-    newTask.innerHTML = `${taskBtn.value}`
-    newBtn.setAttribute('id', 'delete-btn')
+       createTask(inputTask.value)
+    }}
+})
 
-    taskList.appendChild(newTask)
-    newBtn.insert()
-
-    taskBtn.value = ''
-    taskBtn.focus()
+const createTask = (textInput) => {
+    const li = createListItem()
+    li.innerText = textInput
+    taskList.appendChild(li)
+    cleanInput()
+    createButtonDelete(li)
+    saveTasks()
 }
 
-taskAdd.addEventListener('click', addTask)
+const createListItem = () => {
+    return document.createElement("li")
+}
+
+const createButtonDelete = (listItem) => {
+    listItem.innerText += ' '
+    const btnDelete = document.createElement('button')
+    btnDelete.innerText = 'Delete'
+    btnDelete.setAttribute('class', 'delete')
+    listItem.appendChild(btnDelete)
+}
+
+function cleanInput () {
+    inputTask.value = ''
+    inputTask.focus()
+}
+
+btnTask.addEventListener('click', function() {
+    if (!inputTask.value) return
+
+    createTask(inputTask.value)
+})
+
+document.addEventListener('click', function(event) {
+    const element = event.target
+    
+    if (element.classList.contains('delete')) {
+        element.parentElement.remove()
+        saveTasks()
+    }
+}) 
+
+function saveTasks () {
+    const taskCountLi = taskList.querySelectorAll('li')
+    const taskListText = []
+
+    for (let taskIterable of taskCountLi) {
+        let taskText = taskIterable.innerText
+        taskText = taskText.replace('Delete', '').trim()
+        taskListText.push(taskText)
+    }
+
+    const taskListJSON = JSON.stringify(taskListText)
+    localStorage.setItem('tasks', taskListJSON)
+}
+
+const readSavedTasks = () => {
+    const taskSaved = localStorage.getItem('tasks')
+    const taskSavedList = JSON.parse(taskSaved)
+
+    for (let i of taskSavedList) {
+        createTask(i)
+    }
+}
+
+readSavedTasks()
