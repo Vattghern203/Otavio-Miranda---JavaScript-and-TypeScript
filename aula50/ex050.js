1cf9eb6d18b2c387439693b8126a743b8568c03a
vattghern <otavioaugustobrb19@gmail.com>
2022-08-23 15:32:12 -0300

    Funções  Pt.2

---
+++ aula50/ex050.js
@@ -0,0 +1,33 @@
+// argumentos que sustentam todos os argumentos enviados.
+// arguments = todos os argumentos passado para a função, mesmo ela não recebendo nenhum.
+// Mesmo ao passar os argumentos, todos ficam guardados na arguments.
+
+function exFunction (x , y) {
+    let total = 0
+    for (let argumento of arguments) {
+        total += argumento
+    }
+
+    console.log(total, x, y)
+}
+
+exFunction(1, 3, 4, 6, 7, 9, 10)
+
+// Se os argumentos definidos na função excederem os passados, os remanescentes são setados para undefined.
+
+function soma(a, b = 0) {
+    console.log(a + b)
+}
+
+soma(10)
+
+// Caso um parâmetro não for passado ao chamar a função, ele se torna undefined.
+// Para evitar demais problemas, basta setar um valor padrão para ele, caso não seja enviado nada. Exemplo acima.
+
+function soma2(a, b = 10, c = 5) {
+    console.log(a + b + c)
+}
+
+soma2(10, undefined, 10)
+
+// Caso queira que uma parâmetro assuma o valor padrão ele se encontra no meio de vários outros. Basta passar o seu valor como undefined, assim ele receberá o valor padrão.
