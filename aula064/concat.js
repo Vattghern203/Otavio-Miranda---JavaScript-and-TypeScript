c7a46db846d3da2d7c3c903fed68834f10dc8202
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-07-14 11:24:54 -0300

    concat pt.2

--- aula064/concat.js
+++ aula064/concat.js
@@ -4,3 +4,14 @@ const a2 = [4, 5, 6]
 const a3 = [...a1, 'Pão', ...a2, ...[9, 10, 11]]
 
 console.log(a3)
+
+// OU
+
+console.warn("\n----------------- or ----------------\n")
+
+const b1 = [1, 2]
+const b2 = [5, 6]
+
+const b3 = b1.concat([3, 4], b2)
+
+console.log(b3)
